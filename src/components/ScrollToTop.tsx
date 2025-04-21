0bb7b30c88f47e8d21f352a9b6d7137cbc609e8d
nngtkhngoc <khanhngoc230705@gmail.com>
2025-04-21 13:18:36 +0700

    fix scroll to top

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -17,12 +17,12 @@ export default function ScrollToTop() {
   }, []);
   return (
     <button
-      className={`bg-black p-2 rounded-full fixed bottom-10 right-5 z-30 cursor-pointer animate-jump ${
+      className={`bg-secondary p-2 rounded-full fixed bottom-10 right-5 z-30 cursor-pointer animate-jump ${
         isVisible ? "block" : "hidden"
       }`}
       onClick={handleScrollUp}
     >
-      <ChevronsUp className="text-white w-6 h-6" />
+      <ChevronsUp className="text-side w-6 h-6" />
     </button>
   );
 }
